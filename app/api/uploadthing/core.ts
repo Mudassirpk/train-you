03a0d2f76c9c186687c5709a44ac808da7ee7bdb
Mudassir Sarfraz <91904527+Mudassirpk@users.noreply.github.com>
2024-04-27 16:50:03 +0500

    fixed video upload

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -3,18 +3,13 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "4MB" } }).onUploadComplete(
-    async ({ file }: any) => {
-      console.log("file url", file.url);
-      return { success: true };
-    }
-  ),
-  videoUploader: f({ video: { maxFileSize: "16MB" } }).onUploadComplete(
-    async ({ file }: any) => {
-      console.log("file url", file.url);
-      return { success: true };
-    }
-  ),
+  imageUploader: f({
+    image: { maxFileSize: "4MB" },
+    video: { maxFileSize: "1GB" },
+  }).onUploadComplete(async ({ file }: any) => {
+    console.log("file url", file.url);
+    return { success: true };
+  }),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
