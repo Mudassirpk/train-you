ec303baf2601f924637ab1048f62dbc76e2deb10
Mudassir Sarfraz <91904527+Mudassirpk@users.noreply.github.com>
2024-05-08 10:43:12 +0500

    added stripe endpoints

--- app/api/stripe/create-checkout-session/route.ts
+++ app/api/stripe/create-checkout-session/route.ts
@@ -1,6 +1,39 @@
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
- 
-export async function POST(req:NextRequest){
- return NextResponse.json({data:await req.json()})
-}
+import Stripe from "stripe";
 
+export async function POST(req: NextRequest) {
+  const user_session = await getServerSession();
+
+  const data = await req.json();
+
+  const stripeIns = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+  const lineItems = [
+    {
+      price_data: {
+        currency: "pkr",
+        product_data: {
+          name: data.name,
+        },
+        unit_amount: data.price,
+      },
+      quantity: data.quantity,
+    },
+  ];
+
+  if (user_session?.user?.email) {
+    const session = await stripeIns.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: lineItems,
+      mode: "payment",
+      success_url: `http://localhost:3000/details/${data.courseId}?success=true`,
+      cancel_url: `http://localhost:3000/details/${data.courseId}?success=false`,
+      metadata: {
+        courseId: data.courseId,
+        userId: user_session.user.email,
+      },
+    });
+    return NextResponse.json({ session });
+  }
+}
--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -1,5 +1,7 @@
-import { NextResponse,NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export async function POST(req:NextRequest){
-  return NextResponse.json({},{status:200})
+export async function POST(req: NextRequest) {
+
+  // any response sent from here will go to stripe
+  return NextResponse.json({}, { status: 200 });
 }
