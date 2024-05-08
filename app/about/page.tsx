b8ebb8a56c4bcbb95e782a4d0bf33ec83b52e9a4
Mudassir Sarfraz <91904527+Mudassirpk@users.noreply.github.com>
2024-05-08 10:42:27 +0500

    added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,42 @@
+export default function About() {
+  return (
+    <main className="h-screen w-full flex justify-center items-center flex-col">
+      <div className="px-10">
+        <h1 className="text-4xl text-indigo-600 font-bold">
+          <span className="text-indigo-500">About</span> - Train You
+        </h1>
+        <p className="text-lg my-2">
+          <span className="font-bold text-blue-600">Train-You</span> is a
+          cutting-edge online platform that revolutionizes the way individuals
+          pursue their educational and professional development goals. With
+          Train-You, users have unparalleled access to a diverse array of
+          training courses tailored to their specific interests and needs.
+          Whether you're looking to master a new skill, enhance your knowledge
+          in a particular field, or stay ahead in your career,{" "}
+          <span className="font-bold text-blue-600">Train-You</span> offers a
+          comprehensive selection of courses delivered by expert trainers from
+          around the globe. Through our intuitive interface, users can easily
+          browse through various subjects, choose their preferred trainers, and
+          schedule personalized one-on-one sessions via platforms like Zoom for
+          a truly immersive learning experience.
+        </p>
+        <p className="text-lg my-2">
+          Moreover, <span className="font-bold text-blue-600">Train-You</span>{" "}
+          goes beyond just virtual sessions by providing the flexibility of
+          attending scheduled physical or virtual training events. Whether you
+          thrive in face-to-face interactions or prefer the convenience of
+          online learning,{" "}
+          <span className="font-bold text-blue-600">Train-You</span> caters to
+          all preferences, ensuring that every learner can access high-quality
+          education in a manner that suits their lifestyle. Our platform is
+          designed to empower individuals to take control of their learning
+          journey, offering a seamless and engaging experience that fosters
+          growth, skill acquisition, and professional advancement. Join{" "}
+          <span className="font-bold text-blue-600">Train-You</span> today and
+          embark on a transformative educational journey towards realizing your
+          full potential.
+        </p>
+      </div>
+    </main>
+  );
+}
