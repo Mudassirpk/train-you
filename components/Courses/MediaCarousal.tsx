6142c94e2134a01c1286981aa535d03c32ba710b
Mudassir Sarfraz <91904527+Mudassirpk@users.noreply.github.com>
2024-05-08 10:44:22 +0500

    added unique rendering key

--- components/Courses/MediaCarousal.tsx
+++ components/Courses/MediaCarousal.tsx
@@ -21,11 +21,17 @@ export default function MediaCarousal({
           <div className="w-full gap-2 flex">
             {media.map((media) => {
               return media.type.includes("image") ? (
-                <div className="p-2 h-28 aspect-square rounded-lg relative">
+                <div
+                  key={media.url}
+                  className="p-2 h-28 aspect-square rounded-lg relative"
+                >
                   <Image alt="media image" src={media.url} fill={true} />
                 </div>
               ) : (
-                <div className="p-2 h-28 aspect-square rounded-lg relative">
+                <div
+                  key={media.url}
+                  className="p-2 h-28 aspect-square rounded-lg relative"
+                >
                   <video controls={true} className="h-full w-full">
                     <source src={media.url} type={media.type} />
                   </video>
