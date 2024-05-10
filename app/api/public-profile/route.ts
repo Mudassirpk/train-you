476d043eae895ac9d5da139c0f918c68706e57f1
Mudassir Sarfraz <91904527+Mudassirpk@users.noreply.github.com>
2024-05-10 18:27:03 +0500

    added average rattings to user object

--- app/api/public-profile/route.ts
+++ app/api/public-profile/route.ts
@@ -1,17 +1,35 @@
 import connect_db from "@/db/connection";
-import { User } from "@/db/models";
+import { Course, User } from "@/db/models";
+import mongoose from "mongoose";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-
-  const userId = req.nextUrl.searchParams.get('userId')
+  const userId = req.nextUrl.searchParams.get("userId");
 
   await connect_db();
 
-  const user = await User.findOne({ _id: userId }).populate('courses');
+  const user = await User.findOne({ _id: userId }).populate("courses");
 
   if (user) {
+    const courses = await Course.find({
+      teacherId: userId,
+    }).populate("reviews");
+
+    let totalRattings = 0;
+    let sumOfRattings = 0;
+
+    for (let course of courses) {
+      for (let review of course.reviews) {
+        totalRattings += 1;
+        sumOfRattings += parseInt(review.rattings);
+      }
+    }
+
+    const averageRattings = sumOfRattings / totalRattings;
+
     user.password = null;
+    user.details.averageRattings = averageRattings;
+
     return NextResponse.json({
       success: true,
       user,
