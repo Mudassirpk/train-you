6c5751e86f6e9cc3bd614a7f14ed60fa992e41d1
Mudassir Sarfraz <91904527+Mudassirpk@users.noreply.github.com>
2024-05-11 21:48:40 +0500

    removed unused vars

--- app/(admin)/dashboard/courses/course.tsx
+++ app/(admin)/dashboard/courses/course.tsx
@@ -1,10 +1,7 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 
 type Props = {
   title: string;
