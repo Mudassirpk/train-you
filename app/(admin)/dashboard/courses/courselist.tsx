8356da1227902db6656189c10a79b4f4838107af
Mudassir Sarfraz <91904527+Mudassirpk@users.noreply.github.com>
2024-05-11 21:49:00 +0500

    added filter for students while fetching courses

--- app/(admin)/dashboard/courses/courselist.tsx
+++ app/(admin)/dashboard/courses/courselist.tsx
@@ -3,13 +3,23 @@ import Course from "./course";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Loading from "@/components/loading";
+import { useSession } from "next-auth/react";
 
 type Props = {};
 
 function CourseList({}: Props) {
-  const { data, isFetching, isFetched } = useQuery({
+  const session = useSession();
+
+  const { data, isFetching } = useQuery({
     queryKey: ["get-users-courses"],
-    queryFn: async () => await axios.get("/api/course/get-courses"),
+    queryFn: async () =>
+      await axios.get(
+        `/api/course/get-courses${
+          session.data?.user.role === "student"
+            ? "/" + session.data.user._id
+            : ""
+        }`
+      ),
     retry: false,
     refetchOnWindowFocus: false,
   });
