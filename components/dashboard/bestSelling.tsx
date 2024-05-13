fced9fcdb8316da7fd23a5c2418f9d0d002a0db2
Mudassir Sarfraz <91904527+Mudassirpk@users.noreply.github.com>
2024-05-13 15:36:06 +0500

    added dynamic data

--- components/dashboard/bestSelling.tsx
+++ components/dashboard/bestSelling.tsx
@@ -1,33 +1,29 @@
 import Image from "next/image";
 import { Separator } from "../ui/separator";
+import { TCourse } from "@/types/types";
 
-export default function BestSelling() {
+export default function BestSelling({ course }: { course: TCourse }) {
   return (
     <div className="col-span-2 border-2 border-indigo-600 p-4 rounded-lg">
       <div className="flex items-center justify-between">
         <p className="text-lg font-bold text-indigo-600">Best Selling</p>
-        <p className="text-lg font-semibold text-indigo-700 px-4 py-1 rounded-lg bg-indigo-100">
+        {/* <p className="text-lg font-semibold text-indigo-700 px-4 py-1 rounded-lg bg-indigo-100">
           29 Items sold for July
-        </p>
+        </p> */}
       </div>
       <Separator className="my-2" />
       <div className="flex gap-2 items-center">
         <div className="relative min-w-24 h-24">
           <Image
             alt="course-image"
-            src={"/hero.jpg"}
+            src={course.thumbnail}
             fill={true}
             style={{ objectFit: "contain" }}
           />
         </div>
         <div>
-          <p className="text-xl font-bold text-indigo-600">
-            Learn become a successfull person in life
-          </p>
-          <p className="text-indigo-500">
-            Tips to excel in you career. Learn from experienced. Learn time
-            management, preasure handling and many more.
-          </p>
+          <p className="text-xl font-bold text-indigo-600">{course.title}</p>
+          <p className="text-indigo-500">{course.description}</p>
         </div>
       </div>
     </div>
