6d27c253b8905ba249f8fe105a4fbfc168c60345
Mudassir Sarfraz <91904527+Mudassirpk@users.noreply.github.com>
2024-05-13 10:40:58 +0500

    connect to db befor query call

--- app/api/event/route.ts
+++ app/api/event/route.ts
@@ -1,8 +1,11 @@
+import connect_db from "@/db/connection";
 import { Session } from "@/db/models";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const userId = req.nextUrl.searchParams.get("userId");
+
+  await connect_db()
 
   const events = await Session.find({
     author: userId,
