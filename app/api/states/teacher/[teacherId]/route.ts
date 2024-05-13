0b10346ef428218ef41cac4e93ff375a09936faf
Mudassir Sarfraz <91904527+Mudassirpk@users.noreply.github.com>
2024-05-13 15:35:51 +0500

    added latest event state

--- app/api/states/teacher/[teacherId]/route.ts
+++ app/api/states/teacher/[teacherId]/route.ts
@@ -1,10 +1,11 @@
 import connect_db from "@/db/connection";
-import { Course, Enrollment } from "@/db/models";
+import { Course, Earning, Enrollment, Session } from "@/db/models";
+import dayjs from "dayjs";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { teacherId: string } },
+  { params }: { params: { teacherId: string } }
 ) {
   await connect_db();
 
@@ -29,11 +30,51 @@ export async function GET(
     }
   }
 
+  const latestEvent = await Session.findOne({
+    author: params.teacherId,
+  })
+    .sort({
+      createdAt: -1,
+    })
+    .populate({
+      path: "venue",
+      select: "name",
+    })
+    .populate({
+      path: "members",
+      select: "name email",
+    });
+
+  const earnings = await Earning.find().populate({
+    path: "course",
+    match: { teacherId: params.teacherId },
+  });
+
+  const earningsPerCurrentMonth = earnings
+    .filter((earning: any) => {
+      return dayjs(earning.createdAt).format("MMMM") === dayjs().format("MMMM");
+    })
+    .map((e) => {
+      return {
+        amount: e.amount,
+        month: dayjs().format("MMMM"),
+      };
+    });
+
+
+  let ePM = 0;
+
+  for (let earning of earningsPerCurrentMonth) {
+    ePM += earning.amount;
+  }
+
   return NextResponse.json({
     states: {
       enrollments,
       coursesCreated: coursesCreated.length,
       bestSelling,
+      latestEvent,
+      earnings: { amount: ePM, month: dayjs().format("MMMM") },
     },
   });
 }
